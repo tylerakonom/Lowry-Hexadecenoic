cdfbbf2a2daea8a0c4895130217c7c30c740ff1c
tylerakonom <69989091+tylerakonom@users.noreply.github.com>
2020-10-30 16:53:39 -0600

    update to refseq
    Moved away from ENSEMBL genome to refseq

--- R_scripts/Rsubread.R
+++ R_scripts/Rsubread.R
@@ -1,35 +1,40 @@
 ## Running Rsubread for counts:
 
+# $ source /curc/sw/anaconda/default
+# $ conda activate r361
+# $ R
+
 library(Rsubread)
+TMP = "/scratch/summit/tyak9569/hex_acid/rsub/"
 
 files=c(
-'C://GitHub//Lowry-Hexadecenoic//reads//sorted//1h_m1_t.sort.bam',
-'C://GitHub//Lowry-Hexadecenoic//reads//sorted//1h_m1_u.sort.bam',
-'C://GitHub//Lowry-Hexadecenoic//reads//sorted//1h_m2_t.sort.bam',
-'C://GitHub//Lowry-Hexadecenoic//reads//sorted//1h_m2_u.sort.bam',
-'C://GitHub//Lowry-Hexadecenoic//reads//sorted//1h_m3_t.sort.bam',
-'C://GitHub//Lowry-Hexadecenoic//reads//sorted//1h_m3_u.sort.bam',
-'C://GitHub//Lowry-Hexadecenoic//reads//sorted//6h_m1_t.sort.bam',
-'C://GitHub//Lowry-Hexadecenoic//reads//sorted//6h_m1_u.sort.bam',
-'C://GitHub//Lowry-Hexadecenoic//reads//sorted//6h_m2_t.sort.bam',
-'C://GitHub//Lowry-Hexadecenoic//reads//sorted//6h_m2_u.sort.bam',
-'C://GitHub//Lowry-Hexadecenoic//reads//sorted//6h_m3_t.sort.bam',
-'C://GitHub//Lowry-Hexadecenoic//reads//sorted//6h_m3_u.sort.bam')
+'/scratch/summit/tyak9569/hex_acid/aligned/sorted/1h_m1_t.sort.bam',
+'/scratch/summit/tyak9569/hex_acid/aligned/sorted/1h_m1_u.sort.bam',
+'/scratch/summit/tyak9569/hex_acid/aligned/sorted/1h_m2_t.sort.bam',
+'/scratch/summit/tyak9569/hex_acid/aligned/sorted/1h_m2_u.sort.bam',
+'/scratch/summit/tyak9569/hex_acid/aligned/sorted/1h_m3_t.sort.bam',
+'/scratch/summit/tyak9569/hex_acid/aligned/sorted/1h_m3_u.sort.bam',
+'/scratch/summit/tyak9569/hex_acid/aligned/sorted/6h_m1_t.sort.bam',
+'/scratch/summit/tyak9569/hex_acid/aligned/sorted/6h_m1_u.sort.bam',
+'/scratch/summit/tyak9569/hex_acid/aligned/sorted/6h_m2_t.sort.bam',
+'/scratch/summit/tyak9569/hex_acid/aligned/sorted/6h_m2_u.sort.bam',
+'/scratch/summit/tyak9569/hex_acid/aligned/sorted/6h_m3_t.sort.bam',
+'/scratch/summit/tyak9569/hex_acid/aligned/sorted/6h_m3_u.sort.bam')
 
-## Point to GTF file:
+# Point to gtf
 
-gtf=("C://GitHub//Lowry-Hexadecenoic//reads//Mus_musculus.GRCm38.101.gtf")
+gtf=("/projects/lowryc/genome/mouse/fixedgenes.gtf")
 
 ## Output the file:
 
-OUT="C://GitHub//Lowry-Hexadecenoic//outputs//Rsubread//"
+OUT="/scratch/summit/tyak9569/hex_acid/rsub/"
 
 senseunique=featureCounts(files,
 isGTFAnnotationFile = TRUE,
 annot.ext = gtf,
 GTF.attrType = "gene_id",
 allowMultiOverlap = FALSE,
-nthreads = 12,
+nthreads = 36,
 isPairedEnd=TRUE,
 strandSpecific = 2)
 
@@ -37,7 +42,10 @@ write.table(x=data.frame(
 senseunique$annotation[,c("GeneID","Length")],
 senseunique$counts,stringsAsFactors=FALSE),
 file=paste(OUT,"S2_Rsubread_sorted_senseunique.txt"),
-quote=FALSE,sep="\t",row.names=FALSE) 
+quote=FALSE,sep="//t",row.names=FALSE) 
+
+
+
 
 antisenseunique=featureCounts(files,
 isGTFAnnotationFile = TRUE,
@@ -45,11 +53,11 @@ annot.ext = gtf,
 GTF.attrType = "gene_id",
 allowMultiOverlap = TRUE,
 isPairedEnd=TRUE,
-nthreads = 12,
+nthreads = 18,
 strandSpecific = 1)
  
 write.table(x=data.frame(
 antisenseunique$annotation[,c("GeneID","Length")],
 antisenseunique$counts,stringsAsFactors=FALSE),
-file=paste(OUT,"Rsubread_sorted_antisenseunique.txt"),
-quote=FALSE,sep="\t",row.names=FALSE)
+file=paste(OUT,"Rsubread_sorted//_antisenseunique.txt"),
+quote=FALSE,sep="//t",row.names=FALSE)
