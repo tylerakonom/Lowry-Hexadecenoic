c8c2f74e8a8d221d6b0cf5712630b8c627da556f
tylerakonom <69989091+tylerakonom@users.noreply.github.com>
2020-12-04 11:38:25 -0700

    update

--- shell_scripts/raw_counts.sh
+++ shell_scripts/raw_counts.sh
@@ -1,8 +1,10 @@
+cd /scratch/summit/tyak9569/hex_acid/aligned/sorted/
 module purge
 ml singularity/3.6.4
 indirectory=/scratch/summit/tyak9569/hex_acid/aligned/sorted/
 
 for pathandfilename in `ls ${indirectory}*.sort.bam`; do
 name=`basename $pathandfilename .sort.bam`
-singularity run --bind /scratch/summit /projects/lowryc/software/containers/rnaseq.sif samtools flagstat /scratch/summit/tyak9569/hex_acid/aligned/sorted/${name}.sort.bam -O tsv
+echo "flagstat for ${name}" >> flagstatout.txt
+singularity run --bind /scratch/summit /projects/lowryc/software/containers/rnaseq.sif samtools flagstat /scratch/summit/tyak9569/hex_acid/aligned/sorted/${name}.sort.bam >> flagstatout.txt
 done
